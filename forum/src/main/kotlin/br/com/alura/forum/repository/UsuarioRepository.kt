89c73afa6451443a379a53e1e45f6991c08d3683
Melissa <meljblins@gmail.com>
2023-03-28 15:17:41 -0300

    Projeto em andamento

--- forum/src/main/kotlin/br/com/alura/forum/repository/UsuarioRepository.kt
+++ forum/src/main/kotlin/br/com/alura/forum/repository/UsuarioRepository.kt
@@ -4,4 +4,8 @@ import br.com.alura.forum.model.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UsuarioRepository: JpaRepository<Usuario, Long> {
+    fun findByEmail(username: String?): Usuario? {
+
+        return TODO("Provide the return value")
+    }
 }
---
+++ forum/src/main/resources/db/migration/V6__insert_password_usuario.sql
@@ -0,0 +1,2 @@
+UPDATE usuario SET password = '$2a$12$GNSl3.SVq/dM3sxYjsIVR.05Gf3CCNEXrYK8sNgW1j624CRXgUe0.'
+WHERE id = 1
